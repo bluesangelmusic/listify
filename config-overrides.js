138c2773acc1167ca1d67a5449eb1d92d657af59
Eric Toler <eric@bluesangelmusic.com>
2021-06-30 16:53:38 -0500

    Implement working template for v2

--- config-overrides.js
+++ config-overrides.js
@@ -1,2 +1,7 @@
-const { useBabelRc, override } = require('customize-cra')
-module.exports = override(useBabelRc())
+const { override, addWebpackModuleRule } = require('customize-cra')
+module.exports = override(
+  addWebpackModuleRule({
+    test: /\.pug$/i,
+    use: 'raw-loader',
+  })
+)
